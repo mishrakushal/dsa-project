4c74d3c621b63f0a0667bcf7b7fda8ca03bdd092
Kushal Mishra <kuskalm@gmail.com>
2022-01-27 23:08:29 +0530

    applied logic for creating new nodes

--- huffman.cpp
+++ huffman.cpp
@@ -5,13 +5,25 @@
 
 using namespace std;
 
+// create linked list for nodes
 struct Node {
 	char character;
 	int frequency;
 	Node *left, *right;
 };
 
-int main() {
+// creating new tree nodes
+Node* getNode(Node* left, Node* right, char character, int frequency) {
+	Node* node = new Node();
+	node->character = character;
+	node->frequency = frequency;
+	node->left = left;
+	node->right = right;
 
+	return node;
+}
+
+int main() {
+	
 	return 0;
 }
