7e2fd690af8dffd9da4aa82b09b97bef1c2a11e1
Kushal Mishra <kuskalm@gmail.com>
2022-01-28 15:04:30 +0530

    used class for linked list, added encode function

--- huffman.cpp
+++ huffman.cpp
@@ -6,14 +6,24 @@
 using namespace std;
 
 // create linked list for nodes
-struct Node {
-	char character;
-	int frequency;
-	Node *left, *right;
+// struct Node {
+// 	char character;
+// 	int frequency;
+// 	Node *left, *right;
+// };
+
+class Node {
+	public:
+		char character;
+		int frequency;
+		Node* left;
+		Node* right;
+		Node* getNode(Node* left, Node* right, char character, int frequency) {}
 };
 
+// remove function because already implement in the class
 // creating new tree nodes
-Node* getNode(Node* left, Node* right, char character, int frequency) {
+Node::Node* getNode(Node* left, Node* right, char character, int frequency) {
 	Node* node = new Node();
 	node->character = character;
 	node->frequency = frequency;
@@ -30,6 +40,24 @@ struct compare {
 		return l->frequency > r->frequency;
 	}
 };
+
+// ----- ENCODING STEP -----
+// traverse tree and encode the characters
+// store respective Huffman codes in a map
+void encode(Node* root, string encodedString, unordered_map<char, string>& huffmanTree) {
+	// return when pointer reaches beyond terminal leaf
+	if (root == nullptr) return;
+
+	// assign coding to character in map once pointer reaches terminal leaf
+	if (!(root->left || root->right)) {
+		huffmanTree[root->character] = encodedString;
+	}
+
+	// left branch is labelled as 0
+	// right branch is labelled as 1
+	encode(root->left, encodedString + "0", huffmanTree);
+	encode(root->right, encodedString + "1", huffmanTree);
+}
 
 int main() {
 	
